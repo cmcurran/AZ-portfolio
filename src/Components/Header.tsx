47513d926a6693765253d28c0aceec091e70ede7
Charlotte Curran <charlotte@telness.se>
2021-05-18 20:28:06 +0200

    style and add copy for header and header image

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,109 +2,60 @@ import React from "react";
 import styled from "styled-components";
 import { NavLarge } from "./Nav";
 
-const Header = ({
+export const Header = ({
   text,
-  navContent,
 }: {
-  text: string[];
-  navContent: { section: string; title: string }[];
+  text: { top: string[]; mid: string; bottom: string };
 }) => {
   return (
-    <OuterWrapper>
-      {" "}
-      {text.map((item, i) => (
-        <InnerWrapper key={`HeaderRow${i}`}>
-          <Wrapper>
-            {item}
-            {i === text.length - 1 && <BetaTag>beta</BetaTag>}
-          </Wrapper>
-          {i === text.length - 1 && (
-            <NavOuterWrapper>
-              <NavInnerWrapper>
-                <NavLarge content={navContent} />
-              </NavInnerWrapper>
-            </NavOuterWrapper>
-          )}
-        </InnerWrapper>
-      ))}
-    </OuterWrapper>
+    <Wrapper>
+      <div>
+        <div>
+          <Top>{text.top[0]}</Top>
+          <TopAnd>{text.top[1]}</TopAnd>
+        </div>
+        <Mid>{text.mid}</Mid>
+        <Bottom>{text.bottom}</Bottom>
+      </div>
+    </Wrapper>
   );
 };
 
-const NavOuterWrapper = styled.div`
-  position: relative;
-`;
-
-const NavInnerWrapper = styled.div`
-  position: absolute;
-  top: -120%;
-`;
-
-const OuterWrapper = styled.div`
-  height: 100vh;
-
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  justify-content: center;
-
-  @media (min-width: 750px) {
-    min-height: 750px;
-  }
-`;
-
-const InnerWrapper = styled.div`
-  display: flex;
-
-  justify-content: center;
-  width: 65%;
-  @media (max-width: 1249px) {
-    width: 77%;
-  }
-`;
-
 const Wrapper = styled.div`
-  white-space: nowrap;
   width: 100%;
-  text-transform: uppercase;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  font-size: 8.858vw;
+  font-size: calc(var(--vw, 1vw) * 8.858);
+
+  padding: 0 4vw;
+  padding: 0 calc(var(--vw, 1vw) * 4);
+
+  @media (max-width: 749px) {
+    padding: 0 4vw;
+    padding: 0 calc(var(--vw, 1vw) * 4);
+  }
+`;
+
+const Top = styled.span`
   font-family: "Suisse";
-
-  font-size: 7.395vw;
-  font-size: calc(var(--vw, 1vw) * 7.395);
-
-  line-height: 7.395vw;
-  line-height: calc(var(--vw, 1vw) * 7.395);
-
-  @media (max-width: 749px) {
-    font-size: 11.8vw;
-    font-size: calc(var(--vw, 1vw) * 11.8);
-    line-height: 11.8vw;
-    line-height: calc(var(--vw, 1vw) * 11.8);
-  }
-
-  @media (min-width: 750px) and (max-width: 1249px) {
-    font-size: 9.395vw;
-    font-size: calc(var(--vw, 1vw) * 9.395);
-    line-height: 9.395vw;
-    line-height: calc(var(--vw, 1vw) * 9.395);
-  }
 `;
 
-const BetaTag = styled.span`
-  font-family: "MG Mono";
+const TopAnd = styled.span`
   color: #00ff29;
-  font-size: 1.041vw;
-  font-size: calc(var(--vw, 1vw) * 1.041);
-
-  @media (max-width: 749px) {
-    font-size: 2.5vw;
-    font-size: calc(var(--vw, 1vw) * 2.5);
-  }
-
-  @media (min-width: 750px) and (max-width: 1249px) {
-    font-size: 1.3vw;
-    font-size: calc(var(--vw, 1vw) * 1.3);
-  }
+  font-family: "Suisse";
 `;
 
-export default Header;
+const Mid = styled.div`
+  font-family: "SuisseWorks";
+`;
+
+const Bottom = styled.div`
+  font-size: 3.387vw;
+  font-size: calc(var(--vw, 1vw) * 3.387);
+  font-family: "SuisseWorksItalic";
+`;
--- src/TurboSculpture.tsx
+++ src/TurboSculpture.tsx
@@ -1,12 +1,13 @@
 import { useState, useMemo } from "react";
 import styled from "styled-components";
-import Header from "./Components/Header";
+import { Header } from "./Components/Header";
 import SectionWithHeader from "./Components/SectionWithHeader";
 import Nav from "./Components/Nav";
-import Degrowth from "./Copy.js";
+
 import { useScrollPosition } from "./UseScrollPosition.js";
 import { RouteComponentProps } from "@reach/router";
-
+import { turboSculpture } from "./Copy";
+import marleyMaps from "./Images/marleyMaps.png";
 //TODO debounce window listeners?
 
 // if on small view and nav open -> resize over 750 px -> setOpen(false)
@@ -30,42 +31,63 @@ export const TurboSculpture = (_props: RouteComponentProps) => {
 
   return useMemo(
     () => (
-      <Wrapper onClick={() => setHideOnScroll(true)}>
-        <Nav content={Degrowth.nav} show={hideOnScroll} />
+      <OuterWrapper onClick={() => setHideOnScroll(true)}>
+        <Header text={turboSculpture.header} />
+        <ImgWrapper>
+          <img src={marleyMaps} style={{ width: "90%" }} />
+        </ImgWrapper>
 
-        <Header text={Degrowth.header} navContent={Degrowth.nav} />
-        <InnerWrapper>
-          {Degrowth.sections.map((section, i) => {
-            return (
-              <SectionWithHeader
-                key={`SectionWithHeader${i}`}
-                section={section.header.section}
-                header={section.header.title}
-                variant={section.body.variant}
-                body={section.body.copy}
-              />
-            );
-          })}
-        </InnerWrapper>
-      </Wrapper>
+        <Wrapper>
+          <Nav content={turboSculpture.nav} show={hideOnScroll} />
+
+          <InnerWrapper>
+            {turboSculpture.sections.map((section, i) => {
+              return (
+                <SectionWithHeader
+                  key={`SectionWithHeader${i}`}
+                  section={section.header.section}
+                  header={section.header.title}
+                  variant={section.body.variant}
+                  body={section.body.copy}
+                />
+              );
+            })}
+          </InnerWrapper>
+        </Wrapper>
+      </OuterWrapper>
     ),
     [hideOnScroll]
   );
 };
 // linear-gradient(90deg,#03ff54 6%,#F0f0f0 26%)
+const OuterWrapper = styled.div`
+  width: 100%;
+  background-color: #f0f0f0;
+  background-image: linear-gradient(90deg, #03ff54 6%, #f0f0f0 26%);
+`;
+
+const ImgWrapper = styled.div`
+  padding: 0 4vw;
+  padding: 0 calc(var(--vw, 1vw) * 4);
+  display: flex;
+  justify-content: center;
+  @media (max-width: 749px) {
+    padding: 0 4vw;
+    padding: 0 calc(var(--vw, 1vw) * 4);
+  }
+`;
+
 const Wrapper = styled.div`
-  padding-right: 10vw;
-  padding-right: calc(var(--vw, 1vw) * 10);
+  padding: 10vh 4vw;
+  padding: calc(var(--vh, 1vh) * 10) calc(var(--vw, 1vw) * 4);
   display: flex;
   flex-direction: column;
   align-items: flex-end;
 
   @media (max-width: 749px) {
-    padding-right: 10vw;
-    padding-right: calc(var(--vw, 1vw) * 10);
+    padding: 10vh 4vw;
+    padding: calc(var(--vh, 1vh) * 10) calc(var(--vw, 1vw) * 4);
   }
-
-  width: 100%;
 `;
 
 const InnerWrapper = styled.div`
